2ce3d20e4581ce015f2c7a8f96e59877343e4feb
DerTeufel <dominik-kassel@gmx.de>
2017-09-27 15:28:12 +0200

    Add sensor wrapper, based off multiHal by Google

--- sensors/scx15/Android.mk
+++ sensors/scx15/Android.mk
@@ -19,25 +19,26 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_BOARD_PLATFORM),scx15)
 
+# Sensors HAL
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM)
-
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_PROPRIETARY_MODULE := true
 
-LOCAL_MODULE_RELATIVE_PATH := hw
-
-LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS := -DLOG_TAG=\"SensorWrapper\"
 
 LOCAL_SRC_FILES := \
-    sensorswrapper.c \
+    sensorswrapper.cpp \
+    SensorEventQueue.cpp \
 
 LOCAL_SHARED_LIBRARIES := \
     libcutils \
-    libhardware \
+    libdl \
     liblog \
+    libutils \
 
-LOCAL_MODULE_TAGS := optional
+LOCAL_STRIP_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
 
