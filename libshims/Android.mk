9d16f275462491d6a01df01a229611c3f5b310ed
Quang Ngô <ngoquang2708@gmail.com>
2018-04-28 21:17:04 +0700

    ril: move rild shims out of frameworks [2/3]     1. Hex-edit rild blobs in vendor/samsung/vivalto3gvn         sed -i s/property_get/____prop_get/g proprietary/bin/rild         sed -i s/property_set/____prop_set/g proprietary/bin/rild         sed -i s/property_get/____prop_get/g proprietary/lib/libril.so         sed -i s/property_set/____prop_set/g proprietary/lib/libril.so         sed -i s/property_get/____prop_get/g proprietary/lib/libreference-ril_sp.so         sed -i s/property_set/____prop_set/g proprietary/lib/libreference-ril_sp.so     2. Implement ____prop_get/set in libril_shim library     3. Cleanup old framworks java shim and shim hex-edited blobs

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -19,7 +19,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := ril_shim.cpp
-LOCAL_SHARED_LIBRARIES := libbinder
+LOCAL_SHARED_LIBRARIES := libbinder libcutils liblog
 LOCAL_MODULE := libril_shim
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
