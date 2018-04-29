ee971abaaf447f6651d486a24c62ce87e3009c54
Quang Ngô <ngoquang2708@gmail.com>
2018-04-29 09:46:23 +0700

    Shim libstagefright out of frameworks [1/3]   1. Add libstagefright_shim   2. Preload libstagefright_shim   3. Ship default frameworks/av repo

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -32,3 +32,21 @@ LOCAL_MODULE := libgps_shim
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := stagefright_shim.cpp
+LOCAL_C_INCLUDES := \
+    $(TOP)/frameworks/av/include \
+    $(TOP)/frameworks/native/include/media/hardware \
+    $(TOP)/frameworks/native/include/media/openmax
+LOCAL_SHARED_LIBRARIES := \
+    libcamera_client \
+    libstagefright \
+    libstagefright_foundation \
+    liblog
+LOCAL_MODULE := libstagefright_shim
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include $(BUILD_SHARED_LIBRARY)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
