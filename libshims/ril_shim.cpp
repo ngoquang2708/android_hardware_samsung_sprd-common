2f60b8cfa577608ece819897e8da63f3f8c2434f
Quang Ngô <ngoquang2708@gmail.com>
2018-05-01 19:30:48 +0700

    ril: use LD_PRELOAD to override property_set/get instead of hex-edit blobs [1/3]   1. ril_shim: implement property_set/get instead of ____prop_set/get   2. revert to original blobs   3. use LD_PRELOAD to shim rild

--- libshims/ril_shim.cpp
+++ libshims/ril_shim.cpp
@@ -20,6 +20,7 @@
 #include <dlfcn.h>
 
 #include <algorithm>
+#include <exception>
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -30,7 +31,15 @@
 #define LOG_NDEBUG 0
 #define LOG_TAG "RIL_SHIM"
 #include <cutils/log.h>
-#include <cutils/properties.h>
+
+#define PROPERTY_VALUE_MAX 92
+
+static int (*__real_property_set)(const char *, const char *);
+static int (*__real_property_get)(const char *, char *, const char *);
+static void constructor() __attribute__((constructor));
+static void destructor() __attribute__((destructor));
+
+static void *libcutils_handle;
 
 /* { RIL_KEYS, { ANDROID_KEY, ANDROID_VALUE_INDEXES } } */
 static std::unordered_map<std::string, std::pair<std::string, int>> KEY_MAP
@@ -48,6 +57,25 @@ static std::unordered_map<std::string, std::pair<std::string, int>> KEY_MAP
     { "gsm.network.type2",
             { "gsm.network.type", 1 } },
 };
+
+void constructor()
+{
+    bool error = true;
+    if ((libcutils_handle = dlopen("/system/lib/libcutils.so", RTLD_LAZY))) {
+        __real_property_set = (typeof __real_property_set) dlsym(libcutils_handle, "property_set");
+        __real_property_get = (typeof __real_property_get) dlsym(libcutils_handle, "property_get");
+        if (__real_property_set && __real_property_get)
+            error = false;
+    }
+    if (error)
+        throw new std::exception();
+}
+
+void destructor()
+{
+    if (!dlclose(libcutils_handle))
+        throw new std::exception();
+}
 
 static std::vector<std::string> split(const std::string &s, char delim)
 {
@@ -78,13 +106,13 @@ extern "C" int _ZN7android6Parcel13writeString16EPKtj() {
     return _ZN7android6Parcel13writeString16EPKDsj();
 }
 
-extern "C" int ____prop_get(const char *key, char *value, const char *default_value)
+extern "C" int property_get(const char *key, char *value, const char *default_value)
 {
     auto search = KEY_MAP.find(key);
     if (search != KEY_MAP.end()) {
         std::string actual_key = (*search).second.first;
         int index = (*search).second.second;
-        if (property_get(actual_key.c_str(), value, default_value) > 0) {
+        if (__real_property_get(actual_key.c_str(), value, default_value) > 0) {
             std::vector<std::string> v = split(value, ',');
             if (index < v.size())
                 strcpy(value, v[index].c_str());
@@ -93,24 +121,24 @@ extern "C" int ____prop_get(const char *key, char *value, const char *default_va
         }
         return strlen(value);
     }
-    return property_get(key, value, default_value);
+    return __real_property_get(key, value, default_value);
 }
 
-extern "C" int ____prop_set(const char *key, const char *value)
+extern "C" int property_set(const char *key, const char *value)
 {
     auto search = KEY_MAP.find(key);
     if (search != KEY_MAP.end()) {
         std::string actual_key = (*search).second.first;
         int index = (*search).second.second;
         char tmp[PROPERTY_VALUE_MAX];
-        if (property_get(actual_key.c_str(), tmp, "") >= 0) {
+        if (__real_property_get(actual_key.c_str(), tmp, "") >= 0) {
             std::vector<std::string> v = split(tmp, ',');
             if (!(index < v.size()))
                 v.resize(index + 1);
             v[index] = value;
             std::string actual_value = concat(v, ',');
-            return property_set(actual_key.c_str(), actual_value.c_str());
+            return __real_property_set(actual_key.c_str(), actual_value.c_str());
         }
     }
-    return property_set(key, value);
+    return __real_property_set(key, value);
 }
