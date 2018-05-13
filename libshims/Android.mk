a57bb2cd95b2d98bbfdc2a370a5d572d16824075
Quang Ngô <ngoquang2708@gmail.com>
2018-05-13 19:48:25 +0700

    libshims: update for O and move to vendor

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,20 +1,3 @@
-#
-# Copyright (C) 2015 The Android Open Source Project
-# Copyright (C) 2015 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -23,6 +6,7 @@ LOCAL_SHARED_LIBRARIES := libbinder liblog
 LOCAL_CPPFLAGS := -fexceptions
 LOCAL_MODULE := libril_shim
 LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
 
@@ -31,6 +15,7 @@ LOCAL_SRC_FILES := gps_shim.cpp
 LOCAL_SHARED_LIBRARIES := libbinder libgui
 LOCAL_MODULE := libgps_shim
 LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
 
@@ -42,11 +27,13 @@ LOCAL_C_INCLUDES := \
     $(TOP)/frameworks/native/include/media/openmax
 LOCAL_SHARED_LIBRARIES := \
     libcamera_client \
+    libnativewindow \
     libstagefright \
     libstagefright_foundation \
     liblog
 LOCAL_MODULE := libstagefright_shim
 LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
 
