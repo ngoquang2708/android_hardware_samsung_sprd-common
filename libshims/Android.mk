06390cf89993a0adcce47390033c2e50b44b707b
Quang Ngô <ngoquang2708@gmail.com>
2018-06-09 18:37:03 +0700

    shims: ril: use RTLD_NEXT to simplify the code

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -3,7 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := ril_shim.cpp
 LOCAL_SHARED_LIBRARIES := libbinder liblog
-LOCAL_CPPFLAGS := -fexceptions
 LOCAL_MODULE := libril_shim
 LOCAL_MODULE_TAGS := optional
 LOCAL_PROPRIETARY_MODULE := true
