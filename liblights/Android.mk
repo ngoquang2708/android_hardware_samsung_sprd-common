e8b30b65172666a0237b790372e1b4bbe286f571
Quang Ngô <ngoquang2708@gmail.com>
2018-06-17 16:18:48 +0700

    liblights: rework!

--- liblights/Android.mk
+++ liblights/Android.mk
@@ -3,6 +3,8 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
 #
+# Copyright (C) 2018 The LineageOS Project
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -18,19 +20,27 @@
 
 LOCAL_PATH:= $(call my-dir)
 
-ifeq ($(strip $(TARGET_HAS_BACKLIT_KEYS)),false)
-LOCAL_CFLAGS += -DNON_BACKLIT_KEYS
-endif
-
 include $(CLEAR_VARS)
 
-ifeq ($(strip $(TARGET_HAS_BACKLIT_KEYS)),false)
-LOCAL_CFLAGS += -DNON_BACKLIT_KEYS
-endif
-
-LOCAL_SRC_FILES := lights.c
+LOCAL_SRC_FILES := lights.cpp
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
+
+# Set LIGHT_BACKLIGHT, LIGHT_BUTTONS, LIGHT_KEYBOARD, LIGHT_NOTIFICATIONS, LIGHT_ATTENTION
+# for your board if it is supported here.
+
+ifeq (scx15,$(TARGET_BOARD_PLATFORM))
+LOCAL_CFLAGS += -DLIGHT_BACKLIGHT=\"/sys/class/backlight/panel/brightness\"
+endif
+
+ifeq (sc8810,$(TARGET_BOARD_PLATFORM))
+LOCAL_CFLAGS += -DLIGHT_BACKLIGHT=\"/sys/class/backlight/panel/brightness\"
+endif
+
+ifeq (sc8830,$(TARGET_BOARD_PLATFORM))
+LOCAL_CFLAGS += -DLIGHT_BACKLIGHT=\"/sys/class/backlight/panel/brightness\"
+endif
+
 include $(BUILD_SHARED_LIBRARY)
